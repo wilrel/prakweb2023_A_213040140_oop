2347a9a68dc3199a0c80c2ecf6bcf00e8e395dd0
Wildan N <94211883+wilrel@users.noreply.github.com>
2023-10-16 22:54:30 +0700

    Commit

---
+++ constant.php
@@ -0,0 +1,33 @@
+<?php 
+
+// define('NAMA', 'Wildan Nasrulloh Reliyanto');
+// echo NAMA;
+
+// echo "<br>";
+
+// const UMUR = 20;
+// echo UMUR;
+
+// class Coba {
+//     const NAMA = 'Wildan';
+// }
+
+// echo Coba::NAMA;
+
+
+
+// echo __FILE__;
+
+
+// function coba() {
+//     return __FUNCTION__;
+// }
+
+// echo coba();
+
+class Coba {
+    public $kelas = __CLASS__;
+}
+
+$obj = new Coba;
+echo $obj->kelas;
